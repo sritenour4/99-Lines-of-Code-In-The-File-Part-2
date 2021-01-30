4bcd401bcc54f02a8c150af58729f68a65052eca
Shannon Ritenour <shannonritenour@gmail.com>
2021-01-30 15:30:34 -0600

    making progress

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-let friends = ['LUCY', 'ASHLEY', 'STEPHEN', 'EDDIE', 'ASHA'];
+document.addEventListener("DOMContentLoaded", function () {
 
-// loop through friends array and send names to console
-for (let f = 0; f < friends.length; ++f) {
-    console.log(friends[f] + ':')
+    let singButton = document.createElement('button');
+    let singButtonText = document.createTextNode('Sing!');
+    singButton.appendChild(singButtonText);
 
-    // For every friend in the friend array, you will want to console.log the descending chorus of this song 100 times. 
-    // The lyric must include your friend's name, and must be grammatically correct when we get down to 1.  
-    for (let i = 99; i > 0; --i) {
-        if (i == 2) {
-            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[f] +
-                ' strikes one out, clears it all out, ' + (i - 1) + ' line of code in the file');
+    singButton.addEventListener('click', function () {
+        let friendsArray = ['LUCY', 'ASHLEY', 'STEPHEN', 'EDDIE', 'ASHA'];
+
+        // loop through friends array and send names to console
+        for (let i = 0; i < friendsArray.length; ++i) {
+            console.log(friendsArray[i] + ':')
+
+            // For every friend in the friend array, you will want to console.log the descending chorus of this song 100 times. 
+            // The lyric must include your friend's name, and must be grammatically correct when we get down to 1.  
+            for (let j = 99; j > 0; --j) {
+                if (j == 2) {
+                    console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, ' + (j - 1) + ' line of code in the file');
+                }
+                else if (j == 1) {
+                    console.log(j + ' line of code in the file, ' + j + ' line of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, no more lines of code in the file');
+                }
+                else {
+                    console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
+                }
+            }
+            let friend = document.createElement('div');
+            friend.className = 'friend';
+            let h3 = document.createElement('h3');
+            let h3Text = document.createTextNode(friendsArray[i]);
+            h3.appendChild(h3Text);
+            friend.appendChild(h3);
+            document.body.appendChild(friend);
         }
-        else if (i == 1) {
-            console.log(i + ' line of code in the file, ' + i + ' line of code; ' + friends[f] +
-                ' strikes one out, clears it all out, no more lines of code in the file');
-        }
-        else {
-            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[f] +
-                ' strikes one out, clears it all out, ' + (i - 1) + ' lines of code in the file');
-        }
-    }
-}
+
+    })
+
+    document.body.appendChild(singButton);
+
+})
