914d12b32ac88df459b65a815260a0efd8693ecc
Shannon Ritenour <shannonritenour@gmail.com>
2021-01-31 15:29:31 -0600

    finished

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+    // create a button that says 'Sing!'
     let singButton = document.createElement('button');
     let singButtonText = document.createTextNode('Sing!');
     singButton.appendChild(singButtonText);
 
+    // add event listener to button
     singButton.addEventListener('click', function () {
         let friendsArray = ['LUCY', 'ASHLEY', 'STEPHEN', 'EDDIE', 'ASHA'];
 
-        // loop through friends array and send names to console
+        // loop through friends array to create 5 new div elements with class name friend to be added to the page, one for each friend
         for (let i = 0; i < friendsArray.length; ++i) {
-            console.log(friendsArray[i] + ':')
-
-            // For every friend in the friend array, you will want to console.log the descending chorus of this song 100 times. 
-            // The lyric must include your friend's name, and must be grammatically correct when we get down to 1.  
-            for (let j = 99; j > 0; --j) {
-                if (j == 2) {
-                    console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
-                        ' strikes one out, clears it all out, ' + (j - 1) + ' line of code in the file');
-                }
-                else if (j == 1) {
-                    console.log(j + ' line of code in the file, ' + j + ' line of code; ' + friendsArray[i] +
-                        ' strikes one out, clears it all out, no more lines of code in the file');
-                }
-                else {
-                    console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
-                        ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
-                }
-            }
-            let friend = document.createElement('div');
+            let friend = document.createElement('div');    
             friend.className = 'friend';
+            // Put an h3 containing the friend's name in the div
             let h3 = document.createElement('h3');
-            let h3Text = document.createTextNode(friendsArray[i]);
+            let h3Text = document.createTextNode(friendsArray[i] + ":");
             h3.appendChild(h3Text);
             friend.appendChild(h3);
+
+            // outer loop to create grammatically correct paragraphs for each song lyric line and put that paragraph in the friend div
+            for (let j = 99; j > 0; --j) {
+                if (j == 2) {
+                    songLyrics = (j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, ' + (j - 1) + ' line of code in the file');
+
+                    let songParagraph = document.createElement('p');
+                    let songLyricsText = document.createTextNode(songLyrics);
+                    songParagraph.appendChild(songLyricsText);
+                    friend.appendChild(songParagraph);
+                }
+
+                else if (j == 1) {
+                    songLyrics = (j + ' line of code in the file, ' + j + ' line of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, no more lines of code in the file');
+
+                    let songParagraph = document.createElement('p');
+                    let songLyricsText = document.createTextNode(songLyrics);
+                    songParagraph.appendChild(songLyricsText);
+                    friend.appendChild(songParagraph);
+
+                }
+                else {
+                    songLyrics = (j + ' lines of code in the file, ' + j + ' lines of code; ' + friendsArray[i] +
+                        ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
+
+                    let songParagraph = document.createElement('p');
+                    let songLyricsText = document.createTextNode(songLyrics);
+                    songParagraph.appendChild(songLyricsText);
+                    friend.appendChild(songParagraph);
+
+                }
+            }
+
             document.body.appendChild(friend);
         }
 
